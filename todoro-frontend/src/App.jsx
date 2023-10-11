48fd3725bd75fda662c85bf665fd688759c643bd
Tavo Asenjo <tavo@Gustavos-MacBook-Pro-2.local>
2023-10-11 09:44:47 -0400

    header component - initial

--- todoro-frontend/src/App.jsx
+++ todoro-frontend/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
 
 const App = () => {
-  return <h1>Welcome to Todoro</h1>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <h1>Welcome to Todoro Shop</h1>
+      </main>
+    </>
+  );
 };
 
 export default App;
